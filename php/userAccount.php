1056b7a36ee1d1740f9aab3c94d22ac6b0ebeb5b
Javier G <javierguidolin.o@gmail.com>
2019-11-19 00:03:19 -0300

    [Update] Paginas restantes + modulos

---
+++ php/userAccount.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/7c3c4957c1.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="../css/style.css">
+
+  <link rel="shortcut icon" type="image/png" href="../img/bs-favicon.png" />
+  <title> BookStore | Login or Register</title>
+
+</head>
+
+<body>
+
+  <!-- Header -->
+  <header id="header" class="__background-perfil">
+
+    <?php require_once("../modulos/navigationBar.php"); ?>
+
+    <!-- TextHeader -->
+    <section class="__header-perfil">
+      <div class="container">
+        <div class="row">
+          <article class="col-12">
+            <div class="pl-4">
+              <h1 class="text-uppercase text-center">Mi Perfil</h1>
+              <blockquote class="blockquote text-center">
+                <p class="mb-0 text-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="text-dark- blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <!-- EndTextHeader -->
+
+  </header>
+  <!-- EndHeader -->
+
+  <!-- Main -->
+  <main>
+    <section class="container">
+
+      <!-- Path -->
+      <div class="mt-5">
+        <nav aria-label="breadcrumb ">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a class="text-reset" href="../index.php">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Perfil</li>
+          </ol>
+        </nav>
+      </div>
+      <!-- EndPath -->
+
+    </section>
+  </main>
+  <!-- EndMain -->
+
+  <!-- Footer -->
+  <footer class="justify-content-center">
+
+    <div class="__social-links">
+      <div class="container">
+
+        <div class="row py-4 d-flex align-items-center">
+
+          <div class="__social-links col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0">Seguinos en nuestras redes sociales!</h6>
+          </div>
+
+          <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fab fa-facebook-f white-text mr-4"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fab fa-twitter white-text mr-4"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fab fa-google-plus-g white-text mr-4"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fab fa-linkedin-in white-text mr-4"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fab fa-instagram white-text"> </i>
+            </a>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="justify-content-center">
+
+      <?php require_once("../modulos/footer.php"); ?>
+
+    </footer>
+    <!-- Footer -->
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
