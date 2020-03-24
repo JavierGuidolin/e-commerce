e98dba931e577fd1f0b45a1fe8a5d1df7f10247e
Javier G <javierguidolin.o@gmail.com>
2020-03-24 00:18:19 -0300

    Carrito con nuevos estilos agregado

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,183 +1,161 @@
 @extends('layouts.plantilla')
- @section('titulo')
-   Carro de compras
- @endsection
+
+
+@section('styles')
+  <link rel="stylesheet" href="/css/cart.css">
+  <link rel="stylesheet" href="/css/partials/form-controls.css">
+@endsection
+ 
+@section('titulo')
+   Carrito
+@endsection
 
 @section('principal')
-<!-- Header -->
-<header id="header" class="__background-cart">
 
-
-  <!-- TextHeader -->
-  <section class="__header-cart">
-    <div class="container">
-      <div class="row">
-        <article class="col-12">
-          <div class="pl-4">
-            <h1 class="text-uppercase text-center">Carrito de Compras</h1>
-            <blockquote class="blockquote text-center">
-              <p class="mb-0 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer class="text-white-50 blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-            </blockquote>
-          </div>
-        </article>
-      </div>
-    </div>
-  </section>
-  <!-- EndTextHeader -->
-
-</header>
-<!-- EndHeader -->
-
-<!-- Main -->
 <main>
 
-  <section class="container">
+  <section class="__cart my-5">
 
-    <!-- Path -->
-    <div class="mt-5">
-      <nav aria-label="breadcrumb ">
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a class="text-reset" href="../index.php">Home</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Carrito de Compras</li>
-        </ol>
-      </nav>
-    </div>
-    <!-- EndPath -->
-    <div class="px-4 px-lg-0">
+      <div class="container">
+          <div class="card shopping-cart">
+
+              <div class="card-body">
+
+                  <!-- product -->
+                  <div class="row">
+
+                      <div class="col-12 col-md-2">
+                          <div class="text-center mb-3">
+                              <img class="img-responsive __width-img-cart" src="/img/libros-portadas/the-arsonist.webp" alt="prewiew">
+                          </div>
+                      </div>
+
+                      <!-- Title and description-->
+                      <div class="col-12 col-md-6">
+                         <div class="h-100 d-flex flex-column justify-content-center">
+                          <h5 class="__product-name"><strong>The Arsonist</strong></h5>
+                          <h4 class="__product-description">
+                              <small>
+                                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia ipsam
+                                  quae cupiditate necessitatibus animi.
+                              </small>
+                          </h4>
+                         </div>
+                      </div>
+                      <!-- Title and description-->
+
+                      <!-- Options -->
+                      <div class="col-12 col-md-4 row mx-auto d-flex align-items-center flex-md-row">
+                          <div class="col-5" style="padding-top: 5px">
+                              <h6 class="font-weight-bold __product-price">$25.00 x</h6>
+                          </div>
+
+                          <div class="col-4">
+                              <div class="quantity">
+                                  <input type="number" class="form-control text-center __form-input" step="1"
+                                      max="99" min="1" value="1" title="Qty" class="qty">
+                              </div>
+                          </div>
+
+                          <div class="col-3">
+                              <button type="button" class="btn btn-xs __btn">
+                                  <i class="fa fa-trash text-white" aria-hidden="true"></i>
+                              </button>
+                          </div>
+                      </div>
 
 
-<div class="pb-5">
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+                      <!-- Options -->
 
-      <!-- Shopping cart table -->
-      <div class="table-responsive">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col" class="border-0 bg-light">
-                <div class="p-2 px-3 text-uppercase">Producto</div>
-              </th>
-              <th scope="col" class="border-0 bg-light">
-                <div class="py-2 text-uppercase">Precio</div>
-              </th>
-              <th scope="col" class="border-0 bg-light">
-                <div class="py-2 text-uppercase">Cantidad</div>
-              </th>
-              <th scope="col" class="border-0 bg-light">
-                <div class="py-2 text-uppercase">Remover</div>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row" class="border-0">
-                <div class="p-2">
-                  <img src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-1_zrifhn.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
-                  <div class="ml-3 d-inline-block align-middle">
-                    <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">Timex Unisex Originals</a></h5><span class="text-muted font-weight-normal font-italic d-block">Categoria: Relojes</span>
                   </div>
-                </div>
-              </th>
-              <td class="border-0 align-middle"><strong>$79.00</strong></td>
-              <td>
-                        <select class="border-0 align-middle form-control">
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                        </select>
-              </td>
-              <td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
-            </tr>
-            <tr>
-              <th scope="row">
-                <div class="p-2">
-                  <img src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-3_cexmhn.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
-                  <div class="ml-3 d-inline-block align-middle">
-                    <h5 class="mb-0"><a href="#" class="text-dark d-inline-block">Lumix camera lense</a></h5><span class="text-muted font-weight-normal font-italic">Categoria: Electronicos</span>
-                  </div>
-                </div>
-              </th>
-              <td class="align-middle"><strong>$79.00</strong></td>
-              <td>
-                        <select class="border-0 align-middle form-control">
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                        </select>
-              </td>
-              <td class="align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a>
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">
-                <div class="p-2">
-                  <img src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-2_qxjis2.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
-                  <div class="ml-3 d-inline-block align-middle">
-                    <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block">Gray Nike running shoe</a></h5><span class="text-muted font-weight-normal font-italic">Categoria: Fashion</span>
-                  </div>
-                </div>
-                <td class="align-middle"><strong>$79.00</strong></td>
-                <td>
-                          <select class="border-0 align-middle form-control">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                          </select>
-                </td>
-                <td class="align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a>
-                </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!-- End -->
-    </div>
-  </div>
 
-  <div class="row py-5 p-4 bg-white rounded shadow-sm">
-    <div class="col-lg-6">
-      <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Codigo de cupon</div>
-      <div class="p-4">
-        <p class="font-italic mb-4">Si tienes un codigo de cupon, ingresalo en la casilla de abajo</p>
-        <div class="input-group mb-4 border rounded-pill p-2">
-          <input type="text" placeholder="Apply coupon" aria-describedby="button-addon3" class="form-control border-0">
-          <div class="input-group-append border-0">
-            <button id="button-addon3" type="button" class="btn btn-dark px-4 rounded-pill"><i class="fa fa-gift mr-2"></i>Aplicar cupon</button>
+                  <hr>
+                  <!-- Product -->
+
+                   <!-- product -->
+                   <div class="row">
+
+                      <div class="col-12 col-md-2">
+                          <div class="text-center mb-3">
+                              <img class="img-responsive __width-img-cart" src="/img/libros-portadas/the-girl-in-red.webp" alt="prewiew">
+                          </div>
+                      </div>
+
+
+                      <!-- Title and description-->
+                      <div class="col-12 col-md-6">
+                         <div class="h-100 d-flex flex-column justify-content-center">
+                          <h5 class="__product-name"><strong>The Arsonist</strong></h5>
+                          <h4 class="__product-description">
+                              <small>
+                                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia ipsam
+                                  quae cupiditate necessitatibus animi.
+                              </small>
+                          </h4>
+                         </div>
+                      </div>
+                      <!-- Title and description-->
+
+                      <!-- Options -->
+
+
+                      <div class="col-12 col-md-4 row mx-auto d-flex align-items-center flex-md-row">
+                          <div class="col-5">
+                              <h6 class="font-weight-bold __product-price">$25.00 x</h6>
+                          </div>
+
+                          <div class="col-4">
+                              <div class="quantity">
+                                  <input type="number" class="form-control text-center __form-input" step="1"
+                                      max="99" min="1" value="1" title="Qty" class="qty">
+                              </div>
+                          </div>
+
+                          <div class="col-3">
+                              <button type="button" class="btn btn-xs __btn">
+                                  <i class="fa fa-trash text-white" aria-hidden="true"></i>
+                              </button>
+                          </div>
+                      </div>
+
+
+                      <!-- Options -->
+
+                  </div>
+
+                  <hr>
+                  <!-- Product -->
+
+              </div>
+
+              <!-- Cart Options -->
+              <div class="border card-footer">
+                  <div class="row">
+                      <div class="col-8 offset-md-6 col-md-4 offset-lg-7 col-lg-4">
+                          <input type="text" class="form-control __form-input" placeholder="Codigo Cupon">
+                      </div>
+                      <div class="col-4 col-md-2 col-lg-1 pl-lg-0">
+                          <button class="btn __btn" href="">Aplicar</button>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="d-flex justify-content-end align-items-center my-3">
+                  <a href="" class="btn text-uppercase font-weight-bold __btn-checkout">Checkout</a>
+                  <div class="px-3">
+                      Total price: <b>$ 50.00</b>
+                  </div>
+              </div>
+
           </div>
-        </div>
-      </div>
-      <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Instrucciones para el envio</div>
-      <div class="p-4">
-        <p class="font-italic mb-4">If you have some information for the seller you can leave them in the box below</p>
-        <textarea name="" cols="30" rows="2" class="form-control"></textarea>
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">Order summary </div>
-      <div class="p-4">
-        <p class="font-italic mb-4">Los costos de envio y adicionales son calculados basados en los valores de los productos.</p>
-        <ul class="list-unstyled mb-4">
-          <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Subtotal </strong><strong>$390.00</strong></li>
-          <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Envio</strong><strong>$10.00</strong></li>
-          <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Impuestos</strong><strong>$0.00</strong></li>
-          <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total</strong>
-            <h5 class="font-weight-bold">$400.00</h5>
-          </li>
-        </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block">Proceder al checkout</a>
-      </div>
-    </div>
-  </div>
 
-</div>
-</div>
-</div>
+          <!-- Cart Options -->
+
+      </div>
+      </div>
+
   </section>
 
 </main>
+ 
 @endsection
