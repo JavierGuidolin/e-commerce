aaeb2a45310ee2cf8e256f2b98cb71495415f95e
Javier G <javierguidolin.o@gmail.com>
2020-03-24 15:05:32 -0300

    Actualizacion de estilos del carrito

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -11,151 +11,227 @@
 @endsection
 
 @section('principal')
+<main class="my-5">
 
-<main>
+    <div class="container">
+        <div class="row">
 
-  <section class="__cart my-5">
+            <section class="col-12 col-lg-7">
+                <div class="__cart mr-lg-2">
 
-      <div class="container">
-          <div class="card shopping-cart">
+                    <!-- Head Table -->
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <p class="mb-0">Producto</p>
+                        </div>
+                        <div class="col-md-4 row">
+                            <div class="col-5">
+                                <p class="mb-0">Precio</p>
+                            </div>
+                            <div class="col-4">
+                                <p class="mb-0">Cantidad</p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <!-- Head Table -->
 
-              <div class="card-body">
+                    <!-- product -->
+                    <div class="row">
 
-                  <!-- product -->
-                  <div class="row">
+                        <div class="col-12 col-md-2">
+                            <div class="text-center mb-3">
+                                <img class="img-responsive __width-img-cart" src="/img/libros-portadas/the-arsonist.webp" alt="prewiew">
+                            </div>
+                        </div>
 
-                      <div class="col-12 col-md-2">
-                          <div class="text-center mb-3">
-                              <img class="img-responsive __width-img-cart" src="/img/libros-portadas/the-arsonist.webp" alt="prewiew">
-                          </div>
-                      </div>
+                        <!-- Title and description-->
+                        <div class="col-12 col-md-6">
+                            <div class="h-100 d-flex flex-column justify-content-center">
+                                <h5 class="__product-name"><strong>The Arsonist</strong></h5>
+                                <h4 class="__product-description">
+                                        <small>
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi
+                                            mollitia
+                                            ipsam
+                                            quae cupiditate necessitatibus animi.
+                                        </small>
+                                    </h4>
+                            </div>
+                        </div>
+                        <!-- Title and description-->
 
-                      <!-- Title and description-->
-                      <div class="col-12 col-md-6">
-                         <div class="h-100 d-flex flex-column justify-content-center">
-                          <h5 class="__product-name"><strong>The Arsonist</strong></h5>
-                          <h4 class="__product-description">
-                              <small>
-                                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia ipsam
-                                  quae cupiditate necessitatibus animi.
-                              </small>
-                          </h4>
-                         </div>
-                      </div>
-                      <!-- Title and description-->
+                        <!-- Options -->
+                        <div class="col-12 col-md-4 row mx-auto d-flex align-items-center flex-md-row">
+                            <div class="col-4">
+                                <h6 class="font-weight-bold __product-price text-center">$25.00 x</h6>
+                            </div>
 
-                      <!-- Options -->
-                      <div class="col-12 col-md-4 row mx-auto d-flex align-items-center flex-md-row">
-                          <div class="col-5" style="padding-top: 5px">
-                              <h6 class="font-weight-bold __product-price">$25.00 x</h6>
-                          </div>
+                            <div class="col-5">
+                                <div class="quantity">
+                                    <input type="number" class="form-control text-center __form-input" step="1" max="99" min="1" value="1">
+                                </div>
+                            </div>
 
-                          <div class="col-4">
-                              <div class="quantity">
-                                  <input type="number" class="form-control text-center __form-input" step="1"
-                                      max="99" min="1" value="1" title="Qty" class="qty">
-                              </div>
-                          </div>
+                            <div class="col-3">
+                                <button type="button" class="btn btn-sm __btn">
+                                    <i class="fa fa-trash text-white" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
 
-                          <div class="col-3">
-                              <button type="button" class="btn btn-xs __btn">
-                                  <i class="fa fa-trash text-white" aria-hidden="true"></i>
-                              </button>
-                          </div>
-                      </div>
+                        <!-- Options -->
 
+                    </div>
 
-                      <!-- Options -->
+                    <hr>
+                    <!-- Product -->
 
-                  </div>
+                    <!-- product -->
+                    <div class="row">
 
-                  <hr>
-                  <!-- Product -->
+                        <div class="col-12 col-md-2">
+                            <div class="text-center mb-3">
+                                <img class="img-responsive __width-img-cart" src="/img/libros-portadas/the-girl-in-red.webp" alt="prewiew">
+                            </div>
+                        </div>
 
-                   <!-- product -->
-                   <div class="row">
+                        <!-- Title and description-->
+                        <div class="col-12 col-md-6">
+                            <div class="h-100 d-flex flex-column justify-content-center">
+                                <h5 class="__product-name"><strong>The Arsonist</strong></h5>
+                                <h4 class="__product-description">
+                                        <small>
+                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi
+                                            mollitia
+                                            ipsam
+                                            quae cupiditate necessitatibus animi.
+                                        </small>
+                                    </h4>
+                            </div>
+                        </div>
+                        <!-- Title and description-->
 
-                      <div class="col-12 col-md-2">
-                          <div class="text-center mb-3">
-                              <img class="img-responsive __width-img-cart" src="/img/libros-portadas/the-girl-in-red.webp" alt="prewiew">
-                          </div>
-                      </div>
+                        <!-- Options -->
 
+                        <div class="col-12 col-md-4 row mx-auto d-flex align-items-center flex-md-row">
+                            <div class="col-4">
+                                <h6 class="font-weight-bold __product-price text-center">$25.00 x</h6>
+                            </div>
 
-                      <!-- Title and description-->
-                      <div class="col-12 col-md-6">
-                         <div class="h-100 d-flex flex-column justify-content-center">
-                          <h5 class="__product-name"><strong>The Arsonist</strong></h5>
-                          <h4 class="__product-description">
-                              <small>
-                                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi mollitia ipsam
-                                  quae cupiditate necessitatibus animi.
-                              </small>
-                          </h4>
-                         </div>
-                      </div>
-                      <!-- Title and description-->
+                            <div class="col-5">
+                                <div class="quantity">
+                                    <input type="number" class="form-control text-center __form-input" step="1" max="99" min="1" value="1" title="Qty" class="qty">
+                                </div>
+                            </div>
 
-                      <!-- Options -->
+                            <div class="col-3">
+                                <button type="button" class="btn btn-sm __btn">
+                                    <i class="fa fa-trash text-white" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
 
+                        <!-- Options -->
 
-                      <div class="col-12 col-md-4 row mx-auto d-flex align-items-center flex-md-row">
-                          <div class="col-5">
-                              <h6 class="font-weight-bold __product-price">$25.00 x</h6>
-                          </div>
+                    </div>
 
-                          <div class="col-4">
-                              <div class="quantity">
-                                  <input type="number" class="form-control text-center __form-input" step="1"
-                                      max="99" min="1" value="1" title="Qty" class="qty">
-                              </div>
-                          </div>
+                    <hr>
+                    <!-- Product -->
 
-                          <div class="col-3">
-                              <button type="button" class="btn btn-xs __btn">
-                                  <i class="fa fa-trash text-white" aria-hidden="true"></i>
-                              </button>
-                          </div>
-                      </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="pb-1 px-3 font-weight-bold d-flex justify-content-between">
+                                <p class="mb-0 mt-1">Subtotal</p>
+                                <p class="mb-0 mt-1">$ 80.36</p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
 
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="mt-2 mb-2 text-right">
+                                <button class="btn __btn text-uppercase font-weight-bold">Checkout <i class="fas fa-angle-right pl-2"></i> </button>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
 
-                      <!-- Options -->
+                </div>
 
-                  </div>
+            </section>
 
-                  <hr>
-                  <!-- Product -->
+            <!-- Resume -->
+            <section class="col-12  offset-lg-1 col-lg-4">
 
-              </div>
+                <div class="row">
 
-              <!-- Cart Options -->
-              <div class="border card-footer">
-                  <div class="row">
-                      <div class="col-8 offset-md-6 col-md-4 offset-lg-7 col-lg-4">
-                          <input type="text" class="form-control __form-input" placeholder="Codigo Cupon">
-                      </div>
-                      <div class="col-4 col-md-2 col-lg-1 pl-lg-0">
-                          <button class="btn __btn" href="">Aplicar</button>
-                      </div>
-                  </div>
-              </div>
+                    <div class="col-12 col-md-6 col-lg-12">
 
-              <div class="d-flex justify-content-end align-items-center my-3">
-                  <a href="" class="btn text-uppercase font-weight-bold __btn-checkout">Checkout</a>
-                  <div class="px-3">
-                      Total price: <b>$ 50.00</b>
-                  </div>
-              </div>
+                        <div class="__cart-resume p-4">
 
-          </div>
+                            <div class="__resume-title border-bottom">
+                                <p class="text-uppercase mb-0 pb-2 font-weight-bold">order summary</p>
+                            </div>
 
-          <!-- Cart Options -->
+                            <div class="__resume-values">
+                                <div class="pb-1">
+                                    <p class="mb-0 mt-1">Subtotal</p>
+                                    <p class="mb-0 mt-1">$ 80.36</p>
+                                </div>
 
-      </div>
-      </div>
+                                <div class="pb-1">
+                                    <p class="mb-0">Envio</p>
+                                    <p class="mb-0">-</p>
+                                </div>
 
-  </section>
+                                <div class="pb-3">
+                                    <p class="mb-0">Impuestos</p>
+                                    <p class="mb-0">-</p>
+                                </div>
+
+                                <div class="__resume-total border-top pt-2  font-weight-bold">
+                                    <p class="text-uppercase mb-0 pb-2">total</p>
+                                    <p class="text-uppercase mb-0 pb-2">$65.00</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <!-- Coupon -->
+
+                    <div class="col-12 col-md-6 col-lg-12">
+                        <div class="__coupon px-4 py-2 mt-2 mt-md-0 mt-lg-2 h-100 ">
+                            <div class="__resume-title border-bottom my-3 pt-1">
+                                <p class="text-uppercase mb-0 pb-2  font-weight-bold">Cupon de Descuento: </p>
+                            </div>
+
+                            <form class="p-1">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <input type="text" class="form-control __form-input" id="coupon" placeholder="Cupon">
+                                    </div>
+                                    <div class="col-12 text-right">
+                                        <button class="btn __btn text-uppercase font-weight-bold mt-2">aplicar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+
+                    <!-- Coupon -->
+
+                </div>
+
+            </section>
+            <!-- Resume -->
+
+        </div>
+    </div>
 
 </main>
- 
 @endsection
